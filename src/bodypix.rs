21b6ba8b02b60a1363f81c7e7870e673b60c5a18
Filip Joelsson <filip@pixiecoder.com>
2021-05-19 00:13:56 +0200

    Implemented filepicker, encapsulated bodypix in class

--- src/bodypix.rs
+++ src/bodypix.rs
@@ -3,161 +3,56 @@ use std::io::Read;
 use std::ops::Index;
 use std::path::PathBuf;
 
-use tensorflow::{Graph, ImportGraphDefOptions, Session, SessionRunArgs, Tensor};
+use tensorflow::{Graph, ImportGraphDefOptions, Session, SessionOptions, SessionRunArgs, Tensor};
 
 use nannou::image;
 use nannou::image::{DynamicImage, GenericImageView, Pixel};
 
-pub fn load_bodypix_model(assets_path: PathBuf) -> (Graph, u32) {
-    let model_file = assets_path
-        .join("models")
-        .join("bodypix_resnet50_float_model-stride16.pb");
-
-    let stride: u32 = 16;
-
-    let mut model_data: Vec<u8> = Vec::new();
-    let mut file = File::open(model_file).unwrap();
-    file.read_to_end(&mut model_data).unwrap();
-
-    let mut graph = Graph::new();
-
-    let import_results = graph
-        .import_graph_def_with_results(&model_data, &ImportGraphDefOptions::new())
-        .unwrap();
-
-    println!("Results: \n");
-    let outputs = import_results.return_outputs();
-    println!("Return outputs: {}\n", outputs.len());
-    for o in &outputs {
-        println!(
-            "  {}:{} Type: {}",
-            o.operation.name().unwrap(),
-            o.index,
-            o.operation.op_type().unwrap()
-        );
-    }
-    let operations = import_results.return_operations();
-    println!("Return operations: {}\n", operations.len());
-    for o in &operations {
-        println!("  {} Type: {}", o.name().unwrap(), o.op_type().unwrap());
-    }
-    let unused_input_mappings = import_results.missing_unused_input_mappings().unwrap();
-    println!("Unused input mappings: {}\n", unused_input_mappings.len());
-    for (a, b) in &unused_input_mappings {
-        println!("  {} {}", a, b);
-    }
-
-    (graph, stride)
+pub struct BodyPix {
+    graph: Graph,
+    session: Session,
+    stride: u32,
+    // input_operations
+    // output_operations
+    // enum for resnet or mobilenet
 }
 
-pub fn image_to_mask(
-    graph: &mut Graph,
-    session: &mut Session,
-    image: &DynamicImage,
-    stride: u32,
-) -> Vec<Vec<f32>> {
-    fn sigmoid(val: f32) -> f32 {
-        let neg_val = -val;
-        let denominator = 1. + neg_val.exp();
-        1. / denominator
-    }
+impl BodyPix {
+    pub fn from_model(model_path: PathBuf) -> BodyPix {
+        let stride: u32 = 16;
+        let mut model_data: Vec<u8> = Vec::new();
+        let mut file = File::open(model_path).unwrap();
+        file.read_to_end(&mut model_data).unwrap();
 
-    let img_width = image.width();
-    let img_height = image.height();
+        let mut graph = Graph::new();
+        graph
+            .import_graph_def_with_results(&model_data, &ImportGraphDefOptions::new())
+            .unwrap();
 
-    let target_width = img_width + 1;
-    let target_height = img_height + 1;
+        let session = Session::new(&SessionOptions::new(), &graph).unwrap();
 
-    let input_image = image.resize_exact(target_width, target_height, image::imageops::Nearest);
-
-    let target_width = input_image.width();
-    let target_height = input_image.height();
-
-    println!(
-        "{}x{}: {}x{}",
-        img_height, img_width, target_height, target_width
-    );
-
-    // Setup tensor args
-    let vec_size = target_width * target_height * 3;
-    let mut flattened: Vec<f32> = Vec::with_capacity(vec_size as usize);
-
-    for y in 0..target_height {
-        for x in 0..target_width {
-            let pixel = input_image.get_pixel(x as u32, y as u32).to_rgb();
-            // Resnet preprocessing
-            let red = pixel.0[0] as f32 - 123.15;
-            let green = pixel.0[1] as f32 - 115.9;
-            let blue = pixel.0[2] as f32 - 103.06;
-
-            /*
-            // Mobilenet preprocessing
-            let red = pixel.0[0] as f32 / 127.5 - 1.;
-            let green = pixel.0[1] as f32 / 127.5 - 1.;
-            let blue = pixel.0[2] as f32 / 127.5 - 1.;
-            */
-            flattened.push(red);
-            flattened.push(green);
-            flattened.push(blue);
-        }
-    }
-    assert_eq!(flattened.len(), vec_size as usize);
-
-    let input = Tensor::new(&[1, target_height as u64, target_width as u64, 3])
-        .with_values(&flattened)
-        .unwrap();
-
-    println!("Input Image Shape in hwc: {}", input.shape());
-    let width_resolution = (input.shape()[1].unwrap() - 1) as u32 / stride + 1;
-    let height_resolution = (input.shape()[0].unwrap() - 1) as u32 / stride + 1;
-    println!("Resolution: {} {}", width_resolution, height_resolution);
-
-    let mut args: SessionRunArgs = SessionRunArgs::new();
-    args.add_feed(
-        &graph.operation_by_name_required("sub_2").unwrap(),
-        0,
-        &input,
-    );
-
-    let segments_token = args.request_fetch(
-        &graph.operation_by_name_required("float_segments").unwrap(),
-        0,
-    );
-
-    session.run(&mut args).unwrap();
-
-    let segments_res: Tensor<f32> = args.fetch(segments_token).unwrap();
-
-    let segment_height = segments_res.shape().index(1).unwrap() as usize;
-    let segment_width = segments_res.shape().index(2).unwrap() as usize;
-
-    let mut sig_map: Vec<Vec<f32>> = vec![vec![0f32; segment_height]; segment_width];
-    for x in 0..segment_width {
-        for y in 0..segment_height {
-            let seg_val = segments_res.get(&[0, y as u64, x as u64, 0]);
-            sig_map[x][y] = sigmoid(seg_val);
+        BodyPix {
+            stride,
+            graph,
+            session,
         }
     }
 
-    let sig_map = sig_map;
-
-    let blocky_sig = |x: usize, y: usize| -> f32 {
+    pub fn blocky_sig(x: usize, y: usize, stride: u32, sig_map: &Vec<Vec<f32>>) -> f32 {
         let step_x = 1 + x / stride as usize;
         let step_y = 1 + y / stride as usize;
         sig_map[step_x][step_y]
-    };
+    }
 
-    let mean_sig = |x: usize, y: usize| -> f32 {
+    pub fn mean_sig(x: usize, y: usize, stride: u32, sig_map: &Vec<Vec<f32>>) -> f32 {
         let step_x = x / stride as usize;
         let step_y = y / stride as usize;
 
         let x1: usize = step_x;
         let x2: usize = 1 + step_x;
-        assert!(x2 < segment_width);
 
         let y1 = step_y;
         let y2 = 1 + step_y;
-        assert!(y2 < segment_height);
 
         let x_remainder = x as u32 % stride;
         let y_remainder = y as u32 % stride;
@@ -173,15 +68,109 @@ pub fn image_to_mask(
         let mean = mean_x1 * part_x1 + mean_x2 * part_x2;
 
         mean
-    };
-
-    let mut mask: Vec<Vec<f32>> = vec![vec![0.; img_height as usize]; img_width as usize];
-    for x in 0..img_width {
-        for y in 0..img_height {
-            let sig_val = mean_sig(x as usize, y as usize);
-            mask[x as usize][y as usize] = sig_val;
-        }
     }
 
-    mask
+    pub fn process_image(&self, image: &DynamicImage) -> Vec<Vec<f32>> {
+        // TODO: Add mean-func as arg above (either blocky or mean)
+        fn sigmoid(val: f32) -> f32 {
+            let neg_val = -val;
+            let denominator = 1. + neg_val.exp();
+            1. / denominator
+        }
+
+        let img_width = image.width();
+        let img_height = image.height();
+
+        let target_width = img_width + 1;
+        let target_height = img_height + 1;
+
+        // TODO: This resize is superfluous - it could just as well be done in the conversion loop
+        let input_image = image.resize_exact(target_width, target_height, image::imageops::Nearest);
+
+        let target_width = input_image.width();
+        let target_height = input_image.height();
+
+        println!(
+            "{}x{}: {}x{}",
+            img_height, img_width, target_height, target_width
+        );
+
+        // Setup tensor args
+        let vec_size = target_width * target_height * 3;
+        let mut flattened: Vec<f32> = Vec::with_capacity(vec_size as usize);
+
+        for y in 0..target_height {
+            for x in 0..target_width {
+                // TODO: If x == img_width, just get the pixel for (x-1) - and we've merged resize and flatten
+                let pixel = input_image.get_pixel(x as u32, y as u32).to_rgb();
+
+                // TODO: Check which kind of model we have here
+                // Resnet preprocessing
+                let red = pixel.0[0] as f32 - 123.15;
+                let green = pixel.0[1] as f32 - 115.9;
+                let blue = pixel.0[2] as f32 - 103.06;
+
+                /*
+                // Mobilenet preprocessing
+                let red = pixel.0[0] as f32 / 127.5 - 1.;
+                let green = pixel.0[1] as f32 / 127.5 - 1.;
+                let blue = pixel.0[2] as f32 / 127.5 - 1.;
+                */
+                flattened.push(red);
+                flattened.push(green);
+                flattened.push(blue);
+            }
+        }
+        assert_eq!(flattened.len(), vec_size as usize);
+
+        let input = Tensor::new(&[1, target_height as u64, target_width as u64, 3])
+            .with_values(&flattened)
+            .unwrap();
+
+        println!("Input Image Shape in hwc: {}", input.shape());
+        let width_resolution = (input.shape()[1].unwrap() - 1) as u32 / self.stride + 1;
+        let height_resolution = (input.shape()[0].unwrap() - 1) as u32 / self.stride + 1;
+        println!("Resolution: {} {}", width_resolution, height_resolution);
+
+        let mut args: SessionRunArgs = SessionRunArgs::new();
+        args.add_feed(
+            &self.graph.operation_by_name_required("sub_2").unwrap(),
+            0,
+            &input,
+        );
+
+        let segments_token = args.request_fetch(
+            &self
+                .graph
+                .operation_by_name_required("float_segments")
+                .unwrap(),
+            0,
+        );
+
+        self.session.run(&mut args).unwrap();
+
+        let segments_res: Tensor<f32> = args.fetch(segments_token).unwrap();
+
+        let segment_height = segments_res.shape().index(1).unwrap() as usize;
+        let segment_width = segments_res.shape().index(2).unwrap() as usize;
+
+        let mut sig_map: Vec<Vec<f32>> = vec![vec![0f32; segment_height]; segment_width];
+        for x in 0..segment_width {
+            for y in 0..segment_height {
+                let seg_val = segments_res.get(&[0, y as u64, x as u64, 0]);
+                sig_map[x][y] = sigmoid(seg_val);
+            }
+        }
+        let sig_map = sig_map;
+
+        let mut mask: Vec<Vec<f32>> = vec![vec![0.; img_height as usize]; img_width as usize];
+        for x in 0..img_width {
+            for y in 0..img_height {
+                let sig_val = BodyPix::mean_sig(x as usize, y as usize, self.stride, &sig_map);
+                mask[x as usize][y as usize] = sig_val;
+            }
+        }
+
+        mask
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,100 +6,171 @@ use image_utils::*;
 
 use nannou::image::io::Reader as ImageReader;
 use nannou::prelude::*;
+use nannou::ui::prelude::*;
 use nannou::wgpu::Texture;
 
-use tensorflow::{Session, SessionOptions};
+use dirs::picture_dir;
+use nannou::ui::widget::file_navigator::Types::WithExtension;
+use std::path::PathBuf;
 
 fn main() {
     println!("Tensorflow version: {}", tensorflow::version().unwrap());
     nannou::app(model).update(update).simple_window(view).run();
 }
 
-struct Model {
-    image_texture: Option<Texture>,
-    mask_texture: Option<Texture>,
-    silhouette_texture: Option<Texture>,
-    cutout_texture: Option<Texture>,
-}
-
-fn model<'a>(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::Wait);
-
-    let (mut graph, stride) = load_bodypix_model(app.assets_path().unwrap());
-    let mut session = Session::new(&SessionOptions::new(), &graph).unwrap();
-
-    let test_image = ImageReader::open(
-        app.assets_path()
-            .unwrap()
-            .join("images")
-            .join("test-image.jpg"),
-    )
-    .unwrap()
-    .decode()
-    .unwrap();
-    let mask = image_to_mask(&mut graph, &mut session, &test_image, stride);
-    let mask_image = mask_to_image(&mask);
-    let silhouette_image = create_silhouette(&mask, &test_image);
-    let cutout_image = create_cutout(&mask, &test_image);
-
-    let image_texture = Some(Texture::from_image(app, &test_image));
-    let mask_texture = Some(Texture::from_image(app, &mask_image));
-    let silhouette_texture = Some(Texture::from_image(app, &silhouette_image));
-    let cutout_texture = Some(Texture::from_image(app, &cutout_image));
-
-    Model {
-        image_texture,
-        mask_texture,
-        silhouette_texture,
-        cutout_texture,
+widget_ids! {
+    struct Ids {
+        toggle_file_picker,
+        file_picker,
+        process_image,
     }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+struct Model {
+    ui: Ui,
+    ids: Ids,
+
+    show_file_picker: bool,
+    image_file_path: PathBuf,
+
+    body_pix: BodyPix,
+
+    textures: Option<(Texture, Texture, Texture, Texture)>, // image, mask, silhouette, cutout
+}
+
+fn model<'a>(app: &App) -> Model {
+    let mut ui = app.new_ui().build().unwrap();
+    app.set_loop_mode(LoopMode::Wait);
+    let ids = Ids::new(ui.widget_id_generator());
+
+    let body_pix = BodyPix::from_model(
+        app.assets_path()
+            .unwrap()
+            .join("models")
+            .join("bodypix_resnet50_float_model-stride16.pb"),
+    );
+
+    Model {
+        ui,
+        ids,
+
+        show_file_picker: false,
+        image_file_path: PathBuf::new(),
+
+        body_pix,
+
+        textures: (None),
+    }
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+    let Model {
+        ref mut ui,
+        ids,
+        show_file_picker,
+        image_file_path,
+        body_pix,
+        ..
+    } = model;
+
+    let ui = &mut ui.set_widgets();
+
+    let toggle_fp_label = if *show_file_picker {
+        "Cancel"
+    } else {
+        "Pick image"
+    };
+    if widget::Button::new()
+        .top_left()
+        .w_h(150., 30.)
+        .label(toggle_fp_label)
+        .set(ids.toggle_file_picker, ui)
+        .was_clicked()
+    {
+        *show_file_picker = !*show_file_picker;
+    }
+
+    if *show_file_picker {
+        for selection in widget::FileNavigator::new(
+            &*picture_dir().unwrap(),
+            WithExtension(&["jpg", "jpeg", "png", "gif"]),
+        )
+        .w_h(1000., 700.)
+        .set(ids.file_picker, ui)
+        {
+            use nannou::ui::widget::file_navigator::Event::ChangeSelection;
+            match selection {
+                ChangeSelection(vec_pb) => {
+                    if vec_pb.len() > 0 {
+                        let path = vec_pb.last().unwrap();
+                        if path.is_file() {
+                            let p_string = path.clone().into_os_string().into_string().unwrap();
+                            println!("Change selection: {}", p_string);
+                            *image_file_path = path.clone();
+                            *show_file_picker = false;
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    } else if image_file_path.is_file()
+        && widget::Button::new()
+            .label("Process image")
+            .set(ids.process_image, ui)
+            .was_clicked()
+    {
+        let test_image = ImageReader::open(image_file_path)
+            .unwrap()
+            .decode()
+            .unwrap();
+        let mask = body_pix.process_image(&test_image);
+        let mask_image = mask_to_image(&mask);
+        let silhouette_image = create_silhouette(&mask, &test_image);
+        let cutout_image = create_cutout(&mask, &test_image);
+
+        let image_texture = Texture::from_image(app, &test_image);
+        let mask_texture = Texture::from_image(app, &mask_image);
+        let silhouette_texture = Texture::from_image(app, &silhouette_image);
+        let cutout_texture = Texture::from_image(app, &cutout_image);
+
+        model.textures = Some((
+            image_texture,
+            mask_texture,
+            silhouette_texture,
+            cutout_texture,
+        ));
+    }
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     frame.clear(DIMGREY);
     let draw = app.draw();
 
-    match &model.mask_texture {
-        Some(m) => {
-            draw.scale(0.75)
-                .x_y(335., 260.)
-                .texture(&m);
-            ()
-        }
-        None => (),
-    }
+    if model.textures.is_some() {
+        let (image_texture, mask_texture, silhouette_texture, cutout_texture) =
+            model.textures.as_ref().unwrap();
+        draw.x_y(310., 230.)
+            .texture(&mask_texture)
+            .w_h(400., 300.)
+            .finish();
 
-    match &model.image_texture {
-        Some(i) => {
-            draw.scale(0.75)
-                .x_y(-335., 260.)
-                .texture(&i);
-            ()
-        }
-        None => (),
-    }
+        draw.x_y(-90., 230.)
+            .texture(&image_texture)
+            .w_h(400., 300.)
+            .finish();
 
-    match &model.silhouette_texture {
-        Some(m) => {
-            draw.scale(0.75)
-                .x_y(335., -260.)
-                .texture(&m);
-            ()
-        }
-        None => (),
-    }
+        draw.x_y(310., -70.)
+            .texture(&silhouette_texture)
+            .w_h(400., 300.)
+            .finish();
 
-    match &model.cutout_texture {
-        Some(i) => {
-            draw.scale(0.75)
-                .x_y(-335., -260.)
-                .texture(&i);
-            ()
-        }
-        None => (),
+        draw.x_y(-90., -70.)
+            .texture(&cutout_texture)
+            .w_h(400., 300.)
+            .finish();
     }
 
     draw.to_frame(app, &frame).unwrap();
+    model.ui.draw_to_frame(app, &frame).unwrap();
 }
